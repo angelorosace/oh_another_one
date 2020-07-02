4ba2428d7a9c4b662250763c418b59f3d5ba10ea
Angelo Rosace <angelo96rosace@gmail.com>
2020-07-02 18:46:29 +0200

    Updates

---
+++ precache-manifest.fb2b43cdce1087d9e17311d6a2800d34.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c452a637777e1ced12a9b74b06ca7a2",
+    "url": "/oh_another_one/index.html"
+  },
+  {
+    "revision": "9434f0a6d5f99126dd0c",
+    "url": "/oh_another_one/static/css/main.9c5f6051.chunk.css"
+  },
+  {
+    "revision": "369ca9ebe379c0d0b233",
+    "url": "/oh_another_one/static/js/2.601e4a33.chunk.js"
+  },
+  {
+    "revision": "be14b386ccc34a49dee76740150eacbc",
+    "url": "/oh_another_one/static/js/2.601e4a33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9434f0a6d5f99126dd0c",
+    "url": "/oh_another_one/static/js/main.76382dec.chunk.js"
+  },
+  {
+    "revision": "3985a487594dd9c924c9",
+    "url": "/oh_another_one/static/js/runtime-main.db535e53.js"
+  },
+  {
+    "revision": "f7c57689f1ba1eaf05efa921fd686d74",
+    "url": "/oh_another_one/static/media/7_giorni_senza_margine.f7c57689.jpg"
+  },
+  {
+    "revision": "cee3ff31b3ddc4247d36c1d8fed75e8b",
+    "url": "/oh_another_one/static/media/Capricorno.cee3ff31.jpg"
+  },
+  {
+    "revision": "ca5d3f43b47c613b982efa4f3caf6df6",
+    "url": "/oh_another_one/static/media/Ellipse 3.ca5d3f43.svg"
+  },
+  {
+    "revision": "5b11d714a39702d24ba210b401f8bf36",
+    "url": "/oh_another_one/static/media/Keep_watching_keep_fading.5b11d714.jpg"
+  },
+  {
+    "revision": "2d4d6f836a06586074c31f40cd2779d2",
+    "url": "/oh_another_one/static/media/Line 1.2d4d6f83.svg"
+  },
+  {
+    "revision": "28f48a23c45832f18810c23445dccc9c",
+    "url": "/oh_another_one/static/media/Nel_nome_della_rosa_non_squared.28f48a23.png"
+  },
+  {
+    "revision": "415140d1a1ace675938545722d9771c4",
+    "url": "/oh_another_one/static/media/Nel_nome_della_rosa_squared.415140d1.png"
+  },
+  {
+    "revision": "e185c9e1d5280c8a7263e5f3f3e0ef61",
+    "url": "/oh_another_one/static/media/guida_galattica_per_autostoppisti.e185c9e1.png"
+  },
+  {
+    "revision": "6fb5e72a867865b23d778496ad399fa3",
+    "url": "/oh_another_one/static/media/il_barone_rampante.6fb5e72a.jpg"
+  },
+  {
+    "revision": "22c0c275b0eaaa4ed9a776bf1391ad3c",
+    "url": "/oh_another_one/static/media/il_cavaliere_inesistente.22c0c275.jpg"
+  },
+  {
+    "revision": "d41b5d6384f3f048d56932f764fed0f0",
+    "url": "/oh_another_one/static/media/il_maestro_e_margherita.d41b5d63.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/oh_another_one/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2b29dcaf5d880f4000b1dbca6dfaedb9",
+    "url": "/oh_another_one/static/media/logo_black.2b29dcaf.svg"
+  },
+  {
+    "revision": "e356ac96a4141c85f717f2d065f8dd02",
+    "url": "/oh_another_one/static/media/logo_white.e356ac96.svg"
+  },
+  {
+    "revision": "6bf4638b91606ed845d10eb70e52e07b",
+    "url": "/oh_another_one/static/media/mediterraneo.6bf4638b.png"
+  },
+  {
+    "revision": "46c380f1324f57b7ae93a48d904b94fc",
+    "url": "/oh_another_one/static/media/night_single_image.46c380f1.svg"
+  },
+  {
+    "revision": "8d72374ce08734930095f774e656de02",
+    "url": "/oh_another_one/static/media/non_limitarti_a_galleggiare.8d72374c.png"
+  }
+]);
